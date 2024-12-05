0ff065391cf028702189d758f4715c6e6accf35c
Родион Дмитриевич Мосунов <mosunov2003@mail.ru>
2024-12-05 11:32:21 +0500

    Сделан скрипт тестирования алгоритма определения времени по изображениям

--- image_processing_algorithm/__main__.py
+++ image_processing_algorithm/__main__.py
@@ -3,6 +3,7 @@ from concurrent.futures.thread import ThreadPoolExecutor
 
 import cv2
 from image_processing_algorithm.algorithm_debugger import Debugger, DummyDebugger, AlgorithmDebugger
+from image_processing_algorithm.result_analysis import create_report_of_test
 from image_processing_algorithm.utils import Template, Line, ClockTime, check_result, \
     DetectTimeResult
 from pathlib import Path
@@ -10,6 +11,13 @@ from datetime import datetime
 
 
 def detect_time(image_path: Path, debug_mode: None | Debugger = None) -> ClockTime:
+    """
+    Скрипт определения времени на часах. 
+
+    :param image_path: путь к изображению часов
+    :param debug_mode: режим отладки
+    :return: время на часах в формате чч:мм:сс.мс
+    """
     debugger = debug_mode if debug_mode is not None else DummyDebugger()
 
     image = cv2.imread(image_path.as_posix(), cv2.IMREAD_COLOR)
@@ -51,11 +59,27 @@ def detect_time(image_path: Path, debug_mode: None | Debugger = None) -> ClockTi
         'Линия из центра изображения', image, 0, None, [line_1, line_2]
     )
 
+    # Сохранение угла поворота часов
+    clock_angle = 0
+    debugger.save_device_angle(clock_angle)
+
+    # Сохранение результата алгоритма определения времени
     result_time = ClockTime(hours=0, minutes=0, seconds=0, ms=0)
     return result_time
 
 
-def run_test(image_path: Path, folder_for_results: Path, debug_folder: Path, accuracy_sec: int) -> None:
+def _run_test_image(image_path: Path, folder_for_results: Path, debug_folder: Path, accuracy_sec: int) -> None:
+    """
+    Запускает тестирование алгоритма определения времени для 1 изображения. Сохраняет результат в
+    директорию *files/Результат/Результаты*.
+    Выводит в консоль имя файла и получившуюся погрешность при определении времени алгоритмом.
+
+    :param image_path: путь до изображения
+    :param folder_for_results: путь для сохранения промежуточных этапов алгоритма только для тестируемого изображения
+    :param debug_folder: путь до общей папки для сохранения итогового результата
+    :param accuracy_sec: предел максимальной допустимой погрешности
+    :return:
+    """
 
     debug_folder_for_image = debug_folder / image_path.stem
     debug_folder_for_image.mkdir(parents=True, exist_ok=True)
@@ -63,7 +87,7 @@ def run_test(image_path: Path, folder_for_results: Path, debug_folder: Path, acc
     debugger = AlgorithmDebugger(debug_folder_for_image)
 
     result_time = detect_time(image_path, debugger)
-    result_time_dt = datetime.strptime(result_time.__str__(), '%I:%M:%S.%f')
+    result_time_dt = datetime.strptime(result_time.__str__(), '%H:%M:%S.%f')
     excepted_time_24h = datetime.strptime(image_path.stem, '%H:%M:%S.%f')
     excepted_time_dt = datetime.strptime(excepted_time_24h.strftime('%I:%M:%S.%f'), '%I:%M:%S.%f')
 
@@ -79,21 +103,50 @@ def run_test(image_path: Path, folder_for_results: Path, debug_folder: Path, acc
     print(f'{image_path.stem} : погрешность - {delta_sec}')  # noqa: T201
 
 
-def main() -> None:
-    path_to_images_dir = Path('../files/Изображения')
-    debug_folder = Path('../files/Результат')
+def run_tests(path_to_images_dir: Path, debug_folder: Path) -> None:
+    """
+    Запускает тестирование алгоритма определения времени по всем изображения, которые находятся в
+    указанной директории.
+    Результаты различных этапов алгоритма каждого изображения находятся в папке:
+    *files/Результат/Имя тестируемого изображения*
+    Итоговый результат алгоритма по всех изображениям находится в папке: *files/Результат/Результаты*
 
+    Формат имени файла с итоговым результатом через дефис: уложилась ли погрешность в максимальную
+    погрешность, погрешность алгоритма, определенное алгоритмом время, действительное время на
+    изображении, определенный алгоритмом угол поворота циферблата, действительный угол поворота
+    циферблата.
+
+    В результате тестирования в консоли будет выведена таблица, содержащая столбцы: погрешность,
+    процент изображений, уложившихся в погрешность и количество изображений, не уложившихся в
+    погрешность
+
+    :param path_to_images_dir: путь до директории с изображениями с расширением .bmp
+    :param debug_folder: путь до директории для сохранения результатов
+    :return:
+    """
+
+    max_accuracy_sec = 1
     args_list = []
     debug_folder_for_results = debug_folder / "Результаты"
     debug_folder_for_results.mkdir(parents=True, exist_ok=True)
     for image_path in path_to_images_dir.glob('*.bmp'):
-        args_list.append((image_path, debug_folder_for_results, debug_folder))
+        args_list.append((image_path, debug_folder_for_results, debug_folder, max_accuracy_sec))
+        _run_test_image(image_path, debug_folder_for_results, debug_folder, max_accuracy_sec)
 
     assert len(args_list) > 0, 'В директории нет изображений с расширением .bmp'
 
     jobs = 1
     with ThreadPoolExecutor(max_workers=jobs) as executor:
-        executor.map(run_test, *zip(*args_list, strict=False))
+        executor.map(_run_test_image, *zip(*args_list, strict=False))
+
+    list_accuracy = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    create_report_of_test(debug_folder_for_results, list_accuracy)
+
+
+def main() -> None:
+    path_to_images_dir = Path('../files/Изображения')
+    debug_folder = Path('../files/Результат')
+    run_tests(path_to_images_dir, debug_folder)
 
 
 if __name__ == '__main__':
