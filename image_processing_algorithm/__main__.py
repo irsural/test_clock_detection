38ec723ef68c01eda90a409e5a81946b43d1fd83
feelinglight <kolpacc123@mail.ru>
2024-12-09 21:36:51 +0500

    Количество job задается количеством процессоров на пк

--- image_processing_algorithm/__main__.py
+++ image_processing_algorithm/__main__.py
@@ -1,78 +1,16 @@
 import shutil
 from concurrent.futures.thread import ThreadPoolExecutor
+import multiprocessing
 
-import cv2
-from image_processing_algorithm.algorithm_debugger import Debugger, DummyDebugger, AlgorithmDebugger
+from image_processing_algorithm.algorithm_debugger import AlgorithmDebugger
+from image_processing_algorithm.detect_time import detect_time
 from image_processing_algorithm.result_analysis import create_report_of_test
 from image_processing_algorithm.utils import (
-    Template,
-    Line,
-    ClockTime,
     check_result,
     DetectTimeResult,
 )
 from pathlib import Path
 from datetime import datetime
-
-
-def detect_time(image_path: Path, debug_mode: None | Debugger = None) -> ClockTime:
-    """
-    Скрипт определения времени на часах.
-    Ниже приведены примеры как пользоваться дебагером, чтобы сохранять промежуточные результаты
-    работы алгоритма.
-
-    :param image_path: путь к изображению часов
-    :param debug_mode: режим отладки
-    :return: время на часах в формате чч:мм:сс.мс
-    """
-    debugger = debug_mode if debug_mode is not None else DummyDebugger()
-
-    image = cv2.imread(image_path.as_posix(), cv2.IMREAD_COLOR)
-
-    # Сохранение изображения
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    debugger.save_image('Серое изображение', image_gray)
-
-    # Создание изображения центра картинки
-    top_left_image_center = (image.shape[1] // 4, image.shape[0] // 4)
-    bottom_right_image_center = (image.shape[1] // 4 + 300, image.shape[0] // 4 + 300)
-    center_image = image_gray[
-        top_left_image_center[1] : bottom_right_image_center[1],
-        top_left_image_center[0] : bottom_right_image_center[0],
-    ]
-    # Сохранение изображения центра
-    debugger.save_image('Центр изображения', center_image)
-
-    # Обработка для лучшего выделения контуров
-    center_templ_image_canny = cv2.Canny(center_image, 100, 200)
-    # Создание экземпляра шаблона для отрисовки его контуров
-    center_templ = Template(
-        name='Центр изображения',
-        image=center_templ_image_canny,
-        top_left=top_left_image_center,
-        angle_deg=0,
-    )
-    # Отрисовка контуров центра картинки на оригинальном изображении
-    debugger.save_image_with_contours(
-        'Контур центра на изображении', image, 0, [center_templ], None
-    )
-
-    # Создания экземпляров линий
-    image_center = (315, 250)
-    line_1 = Line('Цветная линяя', image_center, 90, 200)
-    line_2 = Line('Цветная линяя', image_center, 210, 200)
-    # Отрисовка линий на оригинальном изображении
-    debugger.save_image_with_contours(
-        'Линия из центра изображения', image, 0, None, [line_1, line_2]
-    )
-
-    # Сохранение угла поворота часов
-    clock_angle = 0
-    debugger.save_device_angle(clock_angle)
-
-    # Сохранение результата алгоритма определения времени
-    result_time = ClockTime(hours=0, minutes=0, seconds=0, ms=0)
-    return result_time
 
 
 def _run_test_image(
@@ -143,8 +81,7 @@ def run_tests(path_to_images_dir: Path, debug_folder: Path) -> None:
 
     assert len(args_list) > 0, 'В директории нет изображений с расширением .bmp'
 
-    jobs = 1
-    with ThreadPoolExecutor(max_workers=jobs) as executor:
+    with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
         executor.map(_run_test_image, *zip(*args_list, strict=False))
 
     list_accuracy = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
@@ -152,8 +89,9 @@ def run_tests(path_to_images_dir: Path, debug_folder: Path) -> None:
 
 
 def main() -> None:
-    path_to_images_dir = Path('../files/Изображения')
-    debug_folder = Path('../files/Результат')
+    repo_root = Path('..')
+    path_to_images_dir = repo_root / 'Изображения'
+    debug_folder = repo_root / 'Результат'
     run_tests(path_to_images_dir, debug_folder)
 
 
---
+++ image_processing_algorithm/detect_time.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import cv2
+
+from image_processing_algorithm.algorithm_debugger import Debugger, DummyDebugger
+from image_processing_algorithm.utils import ClockTime, Template, Line
+
+
+def detect_time(image_path: Path, debug_mode: None | Debugger = None) -> ClockTime:
+    """
+    Скрипт определения времени на часах.
+    Ниже приведены примеры как пользоваться дебагером, чтобы сохранять промежуточные результаты
+    работы алгоритма.
+
+    :param image_path: путь к изображению часов
+    :param debug_mode: режим отладки
+    :return: время на часах в формате чч:мм:сс.мс
+    """
+    debugger = debug_mode if debug_mode is not None else DummyDebugger()
+
+    image = cv2.imread(image_path.as_posix(), cv2.IMREAD_COLOR)
+
+    # Сохранение изображения
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    debugger.save_image('Серое изображение', image_gray)
+
+    # Создание изображения центра картинки
+    top_left_image_center = (image.shape[1] // 4, image.shape[0] // 4)
+    bottom_right_image_center = (image.shape[1] // 4 + 300, image.shape[0] // 4 + 300)
+    center_image = image_gray[
+        top_left_image_center[1] : bottom_right_image_center[1],
+        top_left_image_center[0] : bottom_right_image_center[0],
+    ]
+    # Сохранение изображения центра
+    debugger.save_image('Центр изображения', center_image)
+
+    # Обработка для лучшего выделения контуров
+    center_templ_image_canny = cv2.Canny(center_image, 100, 200)
+    # Создание экземпляра шаблона для отрисовки его контуров
+    center_templ = Template(
+        name='Центр изображения',
+        image=center_templ_image_canny,
+        top_left=top_left_image_center,
+        angle_deg=0,
+    )
+    # Отрисовка контуров центра картинки на оригинальном изображении
+    debugger.save_image_with_contours(
+        'Контур центра на изображении', image, 0, [center_templ], None
+    )
+
+    # Создания экземпляров линий
+    image_center = (315, 250)
+    line_1 = Line('Цветная линяя', image_center, 90, 200)
+    line_2 = Line('Цветная линяя', image_center, 210, 200)
+    # Отрисовка линий на оригинальном изображении
+    debugger.save_image_with_contours(
+        'Линия из центра изображения', image, 0, None, [line_1, line_2]
+    )
+
+    # Сохранение угла поворота часов
+    clock_angle = 0
+    debugger.save_device_angle(clock_angle)
+
+    # Сохранение результата алгоритма определения времени
+    result_time = ClockTime(hours=0, minutes=0, seconds=0, ms=0)
+    return result_time
