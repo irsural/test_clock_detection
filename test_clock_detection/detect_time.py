168b99bcf80bd80fbe142b7236788ee21b2f30d6
Родион Дмитриевич Мосунов <mosunov2003@mail.ru>
2024-12-13 10:54:32 +0500

    Реализован поиск линий по бинарнмоу изображению и конвертация углов поворота линий во время

--- test_clock_detection/detect_time.py
+++ test_clock_detection/detect_time.py
@@ -101,6 +101,58 @@ def _find_best_lines(
     return lines
 
 
+def _angle_to_hours(arrow_angle: float) -> int:
+    """
+    Перевод угла поворота стрелки в часы
+
+    :param arrow_angle: угол поворота стрелки
+    :return: значение стрелки в часах
+    """
+    hours = arrow_angle // 30
+    return int(hours % 12)
+
+
+def _angle_to_minutes_seconds(arrow_angle: float) -> int:
+    """
+    Перевод угла поворота стрелки в секунды или минуты
+
+    :param arrow_angle: угол поворота стрелки
+    :return: значение стрелки в минутах или секундах
+    """
+    min_sec = arrow_angle // 6
+    return int(min_sec % 60)
+
+
+def _angles_to_ms(arrow_angle: float) -> int:
+    """
+    Перевод значения стрелки в миллисекунды
+
+    :param arrow_angle: угол поворота стрелки
+    :return: значение стрелки в миллисекундах
+    """
+    milliseconds = int(arrow_angle / 0.006) % 1000
+    return milliseconds
+
+
+def _convert_angle_to_time(
+    hours_arrow: Line, minutes_arrow: Line, seconds_arrow: Line
+) -> ClockTime:
+    """
+    Перевод углов поворота стрелок в значения времени
+
+    :param hours_arrow: часовая стрелка
+    :param minutes_arrow: минутная стрелка
+    :param seconds_arrow: секундная стрелка
+    :return: время часов
+    """
+    hours = _angle_to_hours(hours_arrow.angle_deg)
+    minutes = _angle_to_minutes_seconds(minutes_arrow.angle_deg)
+    seconds = _angle_to_minutes_seconds(seconds_arrow.angle_deg)
+    milliseconds = _angles_to_ms(seconds_arrow.angle_deg)
+
+    return ClockTime(hours=hours, minutes=minutes, seconds=seconds, ms=milliseconds)
+
+
 def detect_time(
     root_folder: Path, image_path: Path, debug_mode: None | Debugger = None
 ) -> ClockTime:
@@ -123,12 +175,15 @@ def detect_time(
     debugger.save_image('Серое изображение', image_gray)
 
     # Поиск линий на изображении
+    image_binary = cv2.threshold(image_gray, 220, 255, cv2.THRESH_BINARY)[1]
+    debugger.save_image('Бинарное изображение', image_binary)
+    image_binary_rgba = cv2.cvtColor(image_binary, cv2.COLOR_GRAY2BGRA)
+
     image_center = (315, 250)
-    image_gray_rgba = cv2.cvtColor(image_gray, cv2.COLOR_GRAY2BGRA)
-    best_lines = _find_best_lines(image_gray_rgba, image_center, 1, 0, 200, (255, 255, 255, 255))
+    best_lines = _find_best_lines(image_binary_rgba, image_center, 1, 0, 200, (255, 255, 255, 255))
     # Отрисовка линий на оригинальном изображении
-    debugger.save_image_with_lines('Линия из центра изображения', image_gray_rgba, best_lines)
+    debugger.save_image_with_lines('Линия из центра изображения', image_binary_rgba, best_lines)
 
     # Сохранение результата алгоритма определения времени
-    result_time = ClockTime(hours=0, minutes=0, seconds=0, ms=0)
+    result_time = _convert_angle_to_time(best_lines[2], best_lines[1], best_lines[0])
     return result_time
